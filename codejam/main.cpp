d9119a3db47e3ecd52f82ee2c2e0e1b57cc6bde6
hackean <akinwaletimileyin@gmail.com>
2018-03-31 09:14:48 +0100

    Initial Commit

---
+++ codejam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  codejam
+//
+//  Created by hackean on 3/31/18.
+//  Copyright © 2018 hackean. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
